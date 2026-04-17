a22045bc2f58fb646fcf8c5a865ca28ec0dd3e46
rastogialankrit823-ai <rastogialankrit823@gmail.com>
2026-04-17 23:28:07 +0530

    base2

--- server2.cpp
+++ server2.cpp
@@ -14,28 +14,93 @@
 using namespace std;
 ll ps=0;
 map<ll,ll>clients;
-map<ll,ll>user;
 map<str,str>ids;
-void gen(int server,ll cci){
-    sockaddr_in address;
-    socklen_t len=sizeof(address);
-    ll cid=accept(server, (sockaddr*)&address,&len);
-    //int client_sock = accept(server_fd, (sockaddr*)&address, (socklen_t*)&addrlen);
-    clients[cci]=cid;
-    char uid[1024],pass[1024];
-    read(cid, uid, 1024);
-    read(cid, pass, 1024);
-    str cuid(uid),cpass(pass);
-    if(ids.find(cuid)==ids.end())ps=0; 
+str reqread(int client) {
+    str req;
+    char data[1024];
 
-    //return ps;
+    while(req.find("\r\n\r\n") == str::npos) {
+        int n = read(client, data, sizeof(data));
+        if(n <= 0) break;
+        req.append(data, n);
+    }
+    int pos = req.find("Content-Length:");
+    if(pos != str::npos) {
+        int len = stoi(req.substr(pos + 15));
+        int body_start = req.find("\r\n\r\n") + 4;
+
+        while((int)(req.size() - body_start) < len) {
+            int n = read(client, data, sizeof(data));
+            if(n <= 0) break;
+            req.append(data, n);
+        }
+    }
+
+    return req;
 }
-void genc(int server,ll cci){
-    sockaddr_in address;
-    socklen_t len=sizeof(address);
-    ll cid=accept(server,(sockaddr*)&address,&len);
-    user[cci]=cid;
-    
+void respsend(int client, string body, string type="text/plain") {
+    string res =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: " + type + "\r\n"
+        "Connection: close\r\n"
+        "\r\n" + body;
+
+    write(client, res.c_str(), res.size());
+}
+void handler(int server,int cid){
+    str req=reqread(clients[cid]);
+     if(req.find("GET / ") != string::npos || req.find("GET /HTTP") != string::npos) {
+
+        str html =
+        "<!DOCTYPE html>"
+        "<html><body>"
+        "<h2>Login</h2>"
+        "<input id='u' placeholder='user'><br><br>"
+        "<input id='p' type='password' placeholder='pass'><br><br>"
+        "<button onclick='login()'>Login</button>"
+
+        "<script>"
+        "function login(){"
+        "fetch('/login',{"
+        "method:'POST',"
+        "headers:{'Content-Type':'application/json'},"
+        "body:JSON.stringify({"
+        "uid:document.getElementById('u').value,"
+        "pass:document.getElementById('p').value"
+        "})"
+        "})"
+        ".then(r=>r.text()).then(alert);"
+        "}"
+        "</script>"
+
+        "</body></html>";
+
+        respsend(clients[cid], html, "text/html");
+    }
+    if(req.find("POST /login") != string::npos) {
+
+        string body = req.substr(req.find("\r\n\r\n") + 4);
+        str uid = "", pass = "";
+
+        int u = body.find("uid");
+        if(u != string::npos) {
+            uid = body.substr(u + 6);
+            uid = uid.substr(0, uid.find("\""));
+        }
+
+        int p = body.find("pass");
+        if(p != string::npos) {
+            pass = body.substr(p + 7);
+            pass = pass.substr(0, pass.find("\""));
+        }
+        str rep="";
+        if(ids.find(uid)!=ids.end() && ids[uid]==pass){
+            rep="ok";
+        }
+        else rep="dead dude";
+        respsend(clients[cid],rep);
+        close(clients[cid]);
+    }
 }
 
 int main(){
@@ -53,18 +118,12 @@ int main(){
     bool status=true;
     ll cur=0;
     while(status){
-        ll pcur=0;
-        vector<thread>threads1;
-        while(pcur<=100){
-            ps=1;
-            threads1.emplace_back(gen,server,pcur);
-            if(ps==1){
-                cur++;
-            }
-            pcur++;
-        }
-        for(auto &f1:threads1)f1.join();
-
+        sockaddr_in address;
+        socklen_t len=sizeof(address);
+        ll cid=accept(server, (sockaddr*)&address,&len);
+        cur++;
+        clients[cur]=cid;
+        thread(handler,server,cur).detach();
     }
     return 0;
 }
